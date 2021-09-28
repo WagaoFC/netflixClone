fec284408f28d7540cfe89b38c9b3c8a92214839
WagaoFC <wagnerferreira_07@hotmail.com>
2021-09-28 16:31:29 -0300

    ajustes finais

--- src/components/header.js
+++ src/components/header.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import './header.css';
+import { IoMdNotifications } from 'react-icons/io';
+import { FaSearch } from 'react-icons/fa';
 
 export default ({black}) => {
     return (
@@ -9,9 +11,24 @@ export default ({black}) => {
                     <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/200px-Netflix_2015_logo.svg.png" alt="Logo Netflix" />
                 </a>
             </div>
+            <div className="links">
+                <a href="/">Início</a>
+                <a href="/">Séries</a>
+                <a href="/">Filmes</a>
+                <a href="/">Bombando</a>
+                <a href="/">Minha lista</a>
+            </div>
+            <div className="icons">
+                <div className="search">
+                    <FaSearch size={20} />
+                </div>
+                <div className="notifications">
+                    <IoMdNotifications size={20} />
+                </div>
+            </div>
             <div className="header--user">
                 <a href="/">
-                    <img src="https://ih0.redbubble.net/image.618427277.3222/flat,400x400,075,f.u2.jpg" alt="user" />
+                    <img src="https://occ-0-2124-1567.1.nflxso.net/dnm/api/v6/K6hjPJd6cR6FpVELC5Pd6ovHRSk/AAAABUAcaDX2-P091dLEFUNR0mKUnvOFbw0fDUDr0wK-ixdAFlAkZ9K5bwHPEiZ12klt1dkSJpf4R-SIZJDAhLSKDns.png?r=f08" alt="user" />
                 </a>
             </div>
         </header>
